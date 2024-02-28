e6ce5eb2576dfb35b48b07af722f0c19621a8716
maggymbur <mburumaggy62@gmail.com>
2024-02-28 20:50:17 +0300

    add new files

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("btn").onclick = function(){
+    document.getElementById("confirm").innerHTML = "Order placed. Check email for confirmation";
+    document.getElementById("btn").style.display = "none";
+}
